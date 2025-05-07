0a8301dbc8bccd5860c42e2870ed2bc8697dfd87
Nguyen Tri Thanh <nguyentrithanh1311@gmail.com>
2025-05-07 19:56:07 +0700

    Update code 7/5/2025.

---
+++ README.md
@@ -0,0 +1,1 @@
+# mern-potify-clone-app
---
+++ api/src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.listen(PORT, async () => {
+  console.log(`http://localhost:${PORT}`);
+});
