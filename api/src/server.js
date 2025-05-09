b00518e3a86471e5e7c3a22a6f56eb1f2fe6e636
Nguyen Tri Thanh <nguyentrithanh1311@gmail.com>
2025-05-09 17:12:28 +0700

    Update code 9/5/2025.

--- api/src/server.js
+++ api/src/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import { clerkMiddleware } from "@clerk/express";
+import { v2 as cloudinary } from 'cloudinary';
+import path from "path";
+import fileUpload from "express-fileupload";
 import connectDB from "./config/db.config.js";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
@@ -13,10 +16,22 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 app.use(express.json());
-
 app.use(clerkMiddleware());
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: path.join(__dirname, "tmp"),
+  createParentPath: true,
+  limits: 10 * 1024 * 1024 // 10Mb max file size
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
